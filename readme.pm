70aa5438780cd9171a767218d2a58899b869c044
Pelorus Jack <pelorus.jack@nefariousporpoises.com>
2017-08-31 04:56:44 +1000

    Added readme & additional env.sample variable

---
+++ readme.pm
@@ -0,0 +1,26 @@
+
+
+
+# Start an AWS EC2 Server based on Successful build on Travis-CI
+
+
+## deploy using the following commands
+
+```javascript
+git clone https://github.com/pelorusjack/aws_travisci_server_launch
+cd aws_travisci_server_launch
+npm i -g pm2
+pm2 start index.js
+pm2 startup systemd
+```
+
+run the output of this command e.g. 
+```bash
+[PM2] Init System found: systemd
+[PM2] You have to run this command as root. Execute the following command:
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v8.4.0/bin /home/ubuntu/.nvm/versions/node/v8.4.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+```
+
+
+
+
